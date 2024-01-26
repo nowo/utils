921502e4aa7fe327b3285ebb13e5336ac0849f24
cooj <z8888788@163.com>
2024-01-26 16:31:24 +0800

    chore(other): 更新依赖，eslint配置更新，增加数据结构格式与一维数组对象转换方法

---
+++ eslint.config.js
@@ -0,0 +1,87 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+    {
+        // unocss: true,
+        // formatters: true,
+        stylistic: {
+            indent: 4, // 4, or 'tab'
+            quotes: 'single', // or 'double'
+        },
+        // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
+        ignores: [
+            '*.sh',
+            // 'node_modules',
+            'lib',
+            // '*.md',
+            // '*.scss',
+            '*.woff',
+            '*.ttf',
+            '.vscode',
+            // '.idea',
+            // 'dist',
+            'mock',
+            'public',
+            'bin',
+            'build',
+            'index.html',
+            'vite.config.ts.timestamp*',
+            'src/router/layout.ts',
+        ],
+    },
+    {
+        rules: {
+            // 保存代码时缩进4个空格
+            // 'indent': [
+            //     'error',
+            //     4,
+            //     {
+            //         SwitchCase: 1,
+            //     },
+            // ],
+            // 'indent': ['error', 4],
+            'no-console': [
+                'warn',
+                {
+                    allow: ['error', 'warn'],
+                },
+            ],
+            'curly': ['error', 'multi-line', 'consistent'],
+            // 'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+            'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+
+            'jsonc/indent': ['error', 4],
+            'style/indent': ['error', 4],
+            // '@typescript-eslint/brace-style': [
+            //     'error',
+            //     '1tbs',
+            //     // {
+            //     //   "allowSingleLine": true
+            //     // }
+            // ],
+            // '@typescript-eslint/indent': 'off',
+            'ts/no-use-before-define': [
+                'error',
+                {
+                    variables: false,
+                },
+            ],
+            'antfu/if-newline': 'off', // 允许if(a==1) return 1  这种单行的格式
+            'antfu/top-level-function': 'off',
+            'unused-imports/no-unused-vars': [
+                'warn',
+                {
+                    vars: 'all',
+                    // "varsIgnorePattern": "^_",
+                    // "args": "after-used",
+                    // "argsIgnorePattern": "^_"
+                    argsIgnorePattern: '^',
+                },
+            ],
+            // 'max-statements-per-line': ['error', { max: 2 }],
+            'node/prefer-global/process': ['error', 'always'],
+            'function-call-argument-newline': ['error', 'consistent'],
+
+        },
+    },
+)
--- src/common.ts
+++ src/common.ts
@@ -21,7 +21,7 @@ export function types(o?: any) {
 /**
  * async / await 方法调用定时器(延迟执行)
  * @param {number} ms   暂停的时长（单位：毫秒）
- * @returns
+ * @returns  Promise
  * @example
  * ```js
  * await wait(1000);
@@ -37,14 +37,32 @@ export function wait(ms: number) {
 }
 
 /**
- * JSON深度拷贝对象
- * @param  obj - 对象
- * @returns 拷贝的对象
+ * 数据深拷贝方法
+ * @param  data -  原数据
+ * @returns 拷贝后的新数据
  * @example
  * ```js
  * deepClone({ id: 1 })  // { id: 1 }
  * ```
  */
-export function deepClone<T = any>(obj: T): T {
-    return JSON.parse(JSON.stringify(obj))
+export const deepClone = <T = any>(data: T): T => {
+    let newObj: any
+    // 先使用原生自带的深拷贝，出错了就使用自己自定义的方法
+    try {
+        newObj = structuredClone(data)
+    } catch (err) {
+        try {
+            newObj = Array.isArray(data) ? [] : {}
+        } catch (error) {
+            newObj = {}
+        }
+        for (const attr in data) {
+            if (data[attr] && typeof data[attr] === 'object') {
+                newObj[attr] = deepClone(data[attr])
+            } else {
+                newObj[attr] = data[attr]
+            }
+        }
+    }
+    return newObj
 }
